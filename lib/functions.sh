d1d580f526851af5afd2679cef8c0fc60ebbe054
Frede Hundewadt <fhdk.6pmrw@8shield.net>
2024-08-12 16:43:38 +0200

    refactor merge request
    Move architecture check into check_root()
    
    Use single quotes on EOF to prevent variable expansion
    Credit @rodriguezst the original reporter in the forum

--- lib/functions.sh
+++ lib/functions.sh
@@ -195,8 +195,11 @@ check_root () {
         echo "This utility requires root permissions to run"
         exit
     fi
-    # check if x86_64 and qemu-aarch64-static is available
-    check_host_arch
+    # check if host is x86_64 and qemu-aarch64-static is available
+    [[ $(uname -r) == "x86_64" ]] && \
+        [[ $(which qemu-aarch64-static) =~ "qemu-aarch64-static" ]] ||
+            echo "ERROR: This utility requires package 'qemu-user-static-binfmt'\n" && exit 1
+
 }
 
 check_branch () {
@@ -293,16 +296,12 @@ create_rootfs_pkg() {
     $NSPAWN $CHROOTDIR pacman -Syy --noprogressbar $PACMAN_COLORS
 }
 
-check_host_arch() {
-    [[ $(uname -r) == "x86_64" ]] && [[ $(which qemu-aarch64-static) =~ "qemu-aarch64-static" ]] || echo "ERROR: This utility requires package 'qemu-user-static-binfmt'\n" && exit 1
-}
-
 configure_cli_autologin() {
-    mkdir $ROOTFS_IMG/rootfs_$ARCH/etc/systemd/system/getty@tty1.service.d
-    cat << EOF >> $ROOTFS_IMG/rootfs_$ARCH/etc/systemd/system/getty@tty1.service.d/autologin.conf
+    mkdir -p $ROOTFS_IMG/rootfs_$ARCH/etc/systemd/system/getty@tty1.service.d
+    cat << 'EOF' >> $ROOTFS_IMG/rootfs_$ARCH/etc/systemd/system/getty@tty1.service.d/autologin.conf
 [Service]
 ExecStart=
-ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin root \%I \$TERM
+ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin root %I $TERM
 EOF
 }
 
